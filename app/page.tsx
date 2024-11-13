8c18ea8be99b44d3a9f25f946eaa841fd73e487e
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-11-13 16:23:21 +0100

    apos added

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const lusitana = Lusitana({
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1 className="text-blue-500">I am blue</h1>
+     <h1 className="text-blue-500">I &apos;m blue</h1>
      <div
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
 />
